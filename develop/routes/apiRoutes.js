c335f83f2c76fbd06c25e426f49ed8234371faff
Robert Routhier <robrouth92@gmail.com>
2022-06-05 10:35:06 -0700

    cleaned up routes

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -5,15 +5,17 @@ const path = require('path');
 const uuid = require('uuid');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../db/db.json'));
 })
 
 
 router.post('/notes', (req, res) => {
 
-    const currentSaves = fs.readFileSync(path.join(process.cwd(), '/db/db.json'), 'utf8');
+    const currentSaves = 
+    fs.readFileSync(path.join(process.cwd(), '/db/db.json'), 'utf8');
 
-    const newNote = [...currentSaves, {id: uuid(), title: req.body.title, text: req.body.text}];
+    const newNote = 
+    [...currentSaves, {id: uuid(), title: req.body.title, text: req.body.text}];
 
     fs.writeFileSync(path.join(process.cwd(), '/db/db.json'), newNote);
 
--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
 
+
+// HTML GET Requests notes.html file
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.get("/home", (req, res) => {
+
+// HTML GET Requests index.html file
+router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
