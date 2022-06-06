1522e34b56f1c7db87fd725845cbdf75a5b4822e
Robert Routhier <robrouth92@gmail.com>
2022-06-05 20:52:19 -0700

    just delete route left for bonus

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,26 +1,62 @@
-const router = require('express').Router();
-const fs = require('fs');
-const { Module } = require('module');
-const path = require('path');
-const uuid = require('uuid');
+const router = require("express").Router();
+const fs = require("fs");
+const { Module } = require("module");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-})
+console.log("PID: ", process.pid);
 
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../db/db.json"));
+});
 
-router.post('/notes', (req, res) => {
+router.post("/notes", (req, res) => {
+  try {
+    const currentSaves =
+      fs.readFileSync(path.join(__dirname, "/../db/db.json"), "utf8") || "[]";
 
-    const currentSaves = 
-    fs.readFileSync(path.join(process.cwd(), '/db/db.json'), 'utf8');
+    const formattedCurrentSaves = JSON.parse(currentSaves);
 
-    const newNote = 
-    [...currentSaves, {id: uuid(), title: req.body.title, text: req.body.text}];
+    // TODO: Delete from the db.json file
+    // .filter(note => note.id !== req.body.id);
+    const newNote = [
+      ...formattedCurrentSaves,
+      { id: uuidv4(), title: req.body.title, text: req.body.text },
+    ];
 
-    fs.writeFileSync(path.join(process.cwd(), '/db/db.json'), newNote);
+    fs.writeFileSync(
+      path.join(__dirname, "/../db/db.json"),
+      JSON.stringify(newNote)
+    );
 
     console.log(req.body);
     res.json(true);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
- module.exports = router;
+router.delete("/notes/:id", (req, res) => {
+  try {
+    // TODO: Retrieve the id
+    const noteId = req.query.id;
+    const currentSaves =
+      fs.readFileSync(path.join(__dirname, "/../db/db.json"), "utf8") || "[]";
+
+    const formattedCurrentSaves = JSON.parse(currentSaves);
+
+    const newNote = formattedCurrentSaves.filter((note) => note.id !== noteId);
+
+    fs.writeFileSync(
+      path.join(__dirname, "/../db/db.json"),
+      JSON.stringify(newNote)
+    );
+
+    console.log(req.body);
+    res.json(true);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
--- develop/server.js
+++ develop/server.js
@@ -1,19 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const PORT = process.env.PORT || 3000;
 
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`);
 });
-
